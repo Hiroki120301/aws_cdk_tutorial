df5be6e137584c1eb1db202b471ca7b5889b55c1
Hiroki120301 <hxn137@case.edu>
2023-09-12 09:13:31 -0400

    use table viewer for dynamo db

--- lambda/hitcount.py
+++ lambda/hitcount.py
@@ -7,6 +7,7 @@ ddb = boto3.resource('dynamodb')
 table = ddb.Table(os.environ['HITS_TABLE_NAME'])
 _lambda = boto3.client('lambda')
 
+
 def handler(event, context):
     print('request: {}'.format(json.dumps(event)))
     table.update_item(
@@ -14,13 +15,13 @@ def handler(event, context):
         UpdateExpression='ADD hits :incr',
         ExpressionAttributeValues={':incr': 1}
     )
-    
+
     resp = _lambda.invoke(
         FunctionName=os.environ['DOWNSTREAM_FUNCTION_NAME'],
         Payload=json.dumps(event),
     )
-    
+
     body = resp['Payload'].read()
-    
+
     print('downstream response: {}'.format(body))
     return json.loads(body)
